8bd57e49f49b62f06b57689114cb11d9f961888f
liuchen92 <109885487@qq.com>
2025-05-20 19:13:27 +0800

    Update LMER_VPA.R

--- R_code/LMER_VPA.R
+++ R_code/LMER_VPA.R
@@ -1,1 +1,9 @@
 
+model_mro_vap_analysis<-lmerTest::lmer(MRO_SCORE ~ Protist_diversity+Bacteria_diversity+Latitude +Longitude  + (1| Soils),  data )
+require("glmm.hp")
+glmm.hp(model_mro_vap_analysis)
+
+model_mip_vap_analysis<-lmerTest::lmer(MIP_SCORE ~ Protist_diversity+Bacteria_diversity+Latitude +Longitude  + (1| Soils),  data )
+require("glmm.hp")
+glmm.hp(model_mip_vap_analysis)
+
